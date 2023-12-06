f9a5fd82579297ed6b6bbd2694db10235aea3f17
HyperData3434 <76264118+HyperData3434@users.noreply.github.com>
2023-12-06 10:16:38 -0600

    Slight import changes

--- record.py
+++ record.py
@@ -1,13 +1,9 @@
 import simpleaudio as sa
 import pyaudio as pa
-import json
-import threading
 import time
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 
 # Set up webdriver
 chrome_options = Options()
@@ -18,4 +14,3 @@ def listen():
     play = driver.find_element(By.CLASS_NAME, "playStop-28416")
     play.click
     time.sleep(30)
-    
